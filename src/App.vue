a72049485c9f78d9c72e98973cc670c77ee083e9
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-04-08 10:13:49 +0200

    init front-end

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import EventService from '@/services/EventService.js';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data()  {
+    return {
+      news:[]
+    }
+  },
+  created() {
+    this.getNewsData()
+  },
+  methods: {
+    async getNewsData() {
+      EventService.getNews()
+      .then(
+        (news => {
+          this.$set(this, "news", news);
+        }).bind(this)
+      )
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
